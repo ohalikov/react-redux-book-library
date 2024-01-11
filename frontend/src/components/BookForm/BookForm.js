716d58d4f2a8b6070ac2d43be4af18180be185b7
ohalikov <olegway31@gmail.com>
2024-01-11 22:47:01 +0500

    add connectivity between the form and the store

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,30 +1,49 @@
-import { useState } from 'react'
-import './BookForm.css'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/actionCreators';
+
+import './BookForm.css';
 
 export const BookForm = () => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && author) {
       // dispatch actioz
-      console.log(title, author)
-      setTitle('')
-      setAuthor('')
+      const books = {
+        title,
+        author,
+      };
+      console.log(title, author);
+      dispatch(addBook(books));
+      setTitle('');
+      setAuthor('');
     }
-  }
+  };
   return (
     <div className="app-block book-form">
       <h1>BookForm</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='title'>title: </label>
-          <input type='text' id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <label htmlFor="title">title: </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
-          <label htmlFor='author'>author: </label>
-          <input type='text' id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <label htmlFor="author">author: </label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
         </div>
         <button type="submit">Add book</button>
       </form>
