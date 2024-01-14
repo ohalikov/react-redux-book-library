e83b5b768cf7e2ac6ddbf4bde842c92e31c3088d
ohalikov <olegway31@gmail.com>
2024-01-14 19:40:37 +0500

    add random book from Json file

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators';
+import booksData from '../../data/books.json';
 import './BookForm.css';
 
 export const BookForm = () => {
@@ -22,6 +23,19 @@ export const BookForm = () => {
       setAuthor('');
     }
   };
+
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
+
+    const randomBookWithId = {
+      ...randomBook,
+      id: uuidv4(),
+    };
+
+    dispatch(addBook(randomBookWithId));
+  };
+
   return (
     <div className="app-block book-form">
       <h1>BookForm</h1>
@@ -45,6 +59,9 @@ export const BookForm = () => {
           />
         </div>
         <button type="submit">Add book</button>
+        <button type="button" onClick={handleAddRandomBook}>
+          Add Random
+        </button>
       </form>
     </div>
   );
