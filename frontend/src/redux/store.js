985fcc03bea9fc73182c8dfe2f1679b4aa12dfe7
Олег Халиков <o.halikov@ocas.ai>
2024-01-15 10:39:06 +0500

    WIP: add filterSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/reducer';
+import filterReducer from './slices/filterSlice'
 
 export const store = configureStore({
   reducer: {
     books: booksReducer,
+    filter: filterReducer
   },
 });
+
